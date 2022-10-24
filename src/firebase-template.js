069363495524d8b2010af2b44a3f2b7084259ee6
Philippe Clévy <philippe.clevy@infobec.ca>
2022-10-23 22:18:46 -0400

    Set Firebase configuration file

--- src/firebase-template.js
+++ src/firebase-template.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, doc, getDocs, query, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBj2JSJH6uez7QeLkM8RdXwUs6tWZ7dpsw",
-  authDomain: "tnode-915cd.firebaseapp.com",
-  databaseURL: "https://tnode-915cd-default-rtdb.firebaseio.com",
-  projectId: "tnode-915cd",
-  storageBucket: "tnode-915cd.appspot.com",
-  messagingSenderId: "385832865416",
-  appId: "1:385832865416:web:34b9b079b7f973c660593d"
+  apiKey: "****",
+  authDomain: "****",
+  databaseURL: "****",
+  projectId: "****",
+  storageBucket: "****",
+  messagingSenderId: "****",
+  appId: "****",
 };
 
 const app = initializeApp(firebaseConfig);
